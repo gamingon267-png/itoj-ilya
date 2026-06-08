dfa782958b6e27e9bbae7c95d4fad6ec938aad63
gamingon267-png <gamingon267@gmail.com>
2026-06-08 23:29:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
@@ -14,7 +13,12 @@ const firebaseConfig = {
   measurementId: "G-DZ23TER76P"
 };
 
+// Firebase init
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(); // Google login ke liye
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// Login save rahega page refresh ke baad bhi
+setPersistence(auth, browserLocalPersistence);
