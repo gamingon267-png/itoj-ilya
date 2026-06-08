cc814221b111281dbb0dd59c461cf9b58b468a98
gamingon267-png <gamingon267@gmail.com>
2026-06-08 15:44:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,11 +10,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 import {
-  GoogleAuthProvider,
-  signInWithPopup
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import {
   ref,
   uploadBytes,
   getDownloadURL
