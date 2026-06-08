65ef00bc686b10d3c72c8078f3bd70b1eb8b083f
gamingon267-png <gamingon267@gmail.com>
2026-06-08 10:27:54 +0530

    Import Firebase auth and storage modules

---
+++ main.js
@@ -0,0 +1,3 @@
+import { auth, storage } from "./firebase.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
